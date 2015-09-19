e636dcbe8a4fd09cfcbb635a9eac6e09e4fe2651
LUIS ARCE <luisarce5@gmail.com>
2015-09-19 18:17:13 -0400

    HW Unit 4.5 - added code to getWinner fuction

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,14 +35,48 @@ function getComputerMove(move) {
     return move = move || randomPlay();/* Your Expression */
 }
 
-function getWinner(playerMove,computerMove) {
+function getWinner(playerMove,computerMove) { //opening fuction statement
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if (playerMove == 'rock') { // opening first level 'if' statement
+        if (computerMove == 'rock') { // opening second level 'if statement
+            winner = 'tie';
+        } else if (computerMove == 'paper') {
+            winner = 'computer';
+        } else if (computerMove == 'scissors') {
+            winner = 'player';
+        } else {
+            winner = 'error computerMove input';
+        } // closing else statement
+    } else if (playerMove == 'paper') { //opening 'else if' statement
+        
+        if (computerMove == 'rock') {
+            winner = 'player';
+        } else if (computerMove == 'paper') {
+            winner = 'tie';
+        } else if (computerMove == 'scissors') {
+            winner = 'computer';
+        } else {
+            winner = 'error computerMove input';
+        }
+    } else if (playerMove == 'scissors') {
+        if (computerMove == 'rock') {
+            winner = 'computer';
+        } else if (computerMove == 'paper') {
+            winner = 'player';
+        } else if (computerMove == 'scissors') {
+            winner = 'tie';
+        } else {
+            winner = 'error ComputerMove input';
+        } 
+    } else {
+        winner = 'error in inputs';
+        }   
     return winner;
-}
+} // closing function statement
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
