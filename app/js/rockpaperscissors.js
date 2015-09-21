9e1ee783a6d03efd98c5c511a9290acf2739b1d1
LUIS ARCE <luisarce5@gmail.com>
2015-09-21 16:09:12 -0400

    HW Unit 4.5 - modified getWinner fuction

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,46 +35,33 @@ function getComputerMove(move) {
     return move = move || randomPlay();/* Your Expression */
 }
 
-function getWinner(playerMove,computerMove) { //opening fuction statement
+function getWinner(playerMove,computerMove) { // opening fuctions statement
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
-    if (playerMove == 'rock') { // opening first level 'if' statement
-        if (computerMove == 'rock') { // opening second level 'if statement
-            winner = 'tie';
-        } else if (computerMove == 'paper') {
+    if (playerMove == computerMove) {
+        winner = 'tie';
+    } else if (playerMove == 'rock') {
+        if (computerMove == 'paper') { 
             winner = 'computer';
         } else if (computerMove == 'scissors') {
             winner = 'player';
-        } else {
-            winner = 'error computerMove input';
-        } // closing else statement
-    } else if (playerMove == 'paper') { //opening 'else if' statement
-        
+        }
+    } else if (playerMove == 'paper') {
         if (computerMove == 'rock') {
             winner = 'player';
-        } else if (computerMove == 'paper') {
-            winner = 'tie';
         } else if (computerMove == 'scissors') {
             winner = 'computer';
-        } else {
-            winner = 'error computerMove input';
         }
     } else if (playerMove == 'scissors') {
         if (computerMove == 'rock') {
             winner = 'computer';
         } else if (computerMove == 'paper') {
             winner = 'player';
-        } else if (computerMove == 'scissors') {
-            winner = 'tie';
-        } else {
-            winner = 'error ComputerMove input';
-        } 
-    } else {
-        winner = 'error in inputs';
-        }   
+        }
+    }   
     return winner;
 } // closing function statement
 
